673a91a0fd125fe0f1bb9cd4641be9b832550506
kluzko <kluzniakkuba@gmail.com>
2023-01-11 23:15:07 +0100

    change terrible comments in sort algorithms

--- src/alogrithms/insertionSort.ts
+++ src/alogrithms/insertionSort.ts
@@ -2,14 +2,15 @@ import { delay } from '../utils'
 
 export const insertionSort = async (data: number[], updateBars: (counter: number) => void) => {
     for (let i = 1; i < data.length; i++) {
+        // Assign curr element as a key
         let curr = data[i]
+        // Check the prev elements of key and move them one position ahead if they are greater than key
         let j = i - 1
-        // if left val is bigger than current swap them and swap unitl it is the condition is false
         while (j >= 0 && data[j] > curr) {
             data[j + 1] = data[j]
             j--
         }
-        // insert curr elem to the correct pos
+        // insert curr element to correct position
         data[j + 1] = curr
         updateBars(i)
         await delay(1)
--- src/alogrithms/selectionSort.ts
+++ src/alogrithms/selectionSort.ts
@@ -6,12 +6,12 @@ export const selectionSort = async (data: number[], updateBars: (counter: number
     for (let i = 0; i < data.length - 1; i++) {
         minIndex = i
         for (let j = i + 1; j < data.length; j++) {
-            // comapre the current elem with min elem
+            // comapre the current element with min element
             if (data[j] < data[minIndex]) {
                 minIndex = j
             }
         }
-        // swap the current elem with the min elem found
+        // swap the current element with the min element found
         ;[data[i], data[minIndex]] = [data[minIndex], data[i]]
         updateBars(i)
         await delay(1)
